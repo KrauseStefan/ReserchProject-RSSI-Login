bdc4c693a65b25aa21af95deb89e46cdcc7e6957
STKKJ <stefan.krausekjaer@gmail.com>
2014-05-27 19:06:26 +0200

    Doc: Logning with median filter for real

--- data_collectoer/index.js
+++ data_collectoer/index.js
@@ -1,4 +1,6 @@
 var noble = require('noble');
+var moment = require('moment');
+
 
 var peripheral = {
   uuid: "<uuid>",
@@ -18,12 +20,33 @@ var peripheral = {
   rssi: 0
 };
 
+var medianBuffer = [1000, 1000, 1000, 1000, -10000, -10000, -10000];
+
+function calculateMedian(value){
+  medianBuffer.shift();
+
+  medianBuffer.push(value);
+//  console.log(JSON.stringify(medianBuffer));
+//  console.log(JSON.stringify(medianBuffer.slice().sort()));
+
+  return medianBuffer.slice().sort()[3];
+}
+
+console.log("Time,UUID,LocalName,RSSI,MedianRSSI")
+
 noble.on('discover', function(peripheral){
-    if(!peripheral) console.log("empty");
-    console.log(peripheral.advertisement.localName + " : " + peripheral.uuid);
-    
+//    if(!peripheral) console.log("empty");    
+
+
+
     setInterval(function(){
-        console.log(peripheral.advertisement.localName + " : " + peripheral.rssi);
+       var row = moment().format() + "," +
+                 peripheral.uuid + "," +
+                 peripheral.advertisement.localName + "," +
+                 peripheral.rssi + "," +
+                 calculateMedian(peripheral.rssi);
+
+        console.log(row);
     }, 200);
 });
 
