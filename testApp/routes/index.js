655fd2bb07afa8be8ef837cebbc61ae7c36e85d0
stkkj <stefan.krausekjaer@gmail.com>
2014-08-09 19:11:19 +0200

    Feature: Real bluetooth data

--- testApp/routes/index.js
+++ testApp/routes/index.js
@@ -1,6 +1,7 @@
 "use strict";
 var express = require('express');
 var router = express.Router();
+var noble = require('noble');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -21,12 +22,15 @@ router.get('/api/knowenDevices', function(req, res) {
     res.send(knowenDevices);
 });
 
+var blueToothPeripheral = [];
 var rssi = 88;
 var min = 60;
 var max = 100;
 var goingUp = true;
+
 router.post('/api/deviceScan', function(req, res) {
 
+/*
     if(goingUp){
         rssi++;
         if(rssi >= max){
@@ -47,56 +51,20 @@ router.post('/api/deviceScan', function(req, res) {
             rssi: rssi
         }
     ];
+    */
     setTimeout(function(){
-        res.send(foundDevices);
+        res.send(blueToothPeripheral);
+//        res.send(foundDevices);
     }, 500);    
 });
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
-});
-
-/* GET New User page. */
-router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
-});
-
-/* POST to Add User Service */
-router.post('/adduser', function(req, res) {
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-
-    // Set our collection
-    var collection = db.get('usercollection');
-
-    // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
-});
-
 module.exports = router;
+
+noble.on('discover', function(peripheral){
+//    if(!peripheral) console.log("empty");    
+
+    blueToothPeripheral.push(peripheral);
+
+});
+
+noble.startScanning(); // any service UUID, no duplicates
