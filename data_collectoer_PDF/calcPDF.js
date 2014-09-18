6eb5818703c1df480939137927fc0df081254c61
root <root@raspberrypi.(none)>
2014-09-18 15:22:10 +0200

    Moved PDF data into seperate folder

---
+++ data_collectoer_PDF/calcPDF.js
@@ -0,0 +1,68 @@
+// node samples/sample.js
+
+var parse = require('csv').parse;
+var fs = require('fs');
+
+var TEST_TIME = 20; //minutes
+
+function parseData(name, err, data){
+    if(err){
+        console.log(name, err);
+        return err;
+    }
+    var minute = 60 * 1000;
+    var startTime = new Date(data[0].Time).getTime();
+    var endTime = startTime + TEST_TIME * minute;
+    for(var i = 0; data.length; i++){
+        if(new Date(data[i].Time).getTime() >= endTime){
+            data = data.slice(0, i);
+            break;
+        }
+    }
+
+    data = data.map(function(elm){
+        return elm.RSSI;
+    });
+
+    var dataMap = {};
+    data = data.sort();
+
+    while(data.length > 0){
+    	var value = data.pop();
+    	if(dataMap[value]){
+    		dataMap[value]++;
+    	}else{
+    		dataMap[value] = 1;
+    	}
+    }
+
+    var totalSum = 0;
+    for(var i in dataMap){
+        totalSum += dataMap[i];
+    }
+
+    for(var i = 0 in dataMap){
+	    console.log(i + ',' + dataMap[i] / totalSum);
+    }
+
+}
+
+console.log("value,Number");
+
+var fileName = __dirname + "/" + process.argv[2];
+var rs = fs.createReadStream(fileName);
+parser = parse({columns: true}, parseData.bind(this, fileName));
+rs.pipe(parser);
+
+
+// fs.readdirSync(__dirname).forEach(function(elm){
+//     if(!elm.match(/.csv$/)){
+//         return;
+//     }
+
+//     var rs = fs.createReadStream(__dirname+ "/" +elm);
+
+//     parser = parse({columns: true}, parseData.bind(this, elm));
+//     rs.pipe(parser);
+
+// });
---
+++ data_collectoer_PDF/index.js
@@ -0,0 +1,78 @@
+var noble = require('noble');
+var moment = require('moment');
+
+var updateInterval = 200; //ms
+var bufferSize = 7;
+
+// var peripheral = {
+//   uuid: "<uuid>",
+//   advertisement: {
+//     localName: "<name>",
+//     txPowerLevel: "<int>",
+//     serviceUuids: ["<service UUID>", "more"],
+//     manufacturerData: "<Buffer>",
+//     serviceData: [
+//         {
+//             uuid: "<service UUID>",
+//             data: ["<buffer>"]
+//         },
+//         "..."
+//     ]
+//   },
+//   rssi: 0
+// };
+
+var medianBuffer = [];
+
+function calculateMedian(value){
+  if(medianBuffer.length >= bufferSize){
+    medianBuffer.shift();
+  }
+
+  medianBuffer.push(value);
+  var sorted =  medianBuffer.slice().sort();
+  if(medianBuffer.length % 2 !== 0){ //uneven
+    var i = Math.ceil(medianBuffer.length / 2) -1; //-1 because zero indexed
+    return sorted[i];
+  }else{ //even
+    var i = medianBuffer.length / 2;
+    return (sorted[i - 1] + sorted[i]) / 2; //-1 because zero indexed
+  }
+}
+var UUID = process.argv[2] || '5A:B2:84:B8:36:6D';
+var lockedUuid = UUID.replace(/:/g, '').toLowerCase();
+var count = 0;
+console.log('Time,UUID,LocalName,RSSI,MedianRSSI')
+noble.addListener('discover', function(peripheral){
+  if(!lockedUuid){
+    lockedUuid = peripheral.uuid;
+    console.log(peripheral.toString());
+  }else if(peripheral.uuid !== lockedUuid){
+    return;
+  }
+
+  var row = [moment().format(),
+            peripheral.uuid,
+            peripheral.advertisement.localName | 'Unknowen',
+            peripheral.rssi,
+            calculateMedian(peripheral.rssi)].join(',');
+
+  console.log(row);
+  count++;
+
+  if(count >= 60){
+    process.exit(0);
+  }
+
+});
+
+var scanForDevices = [];
+var allowDoublicates = true;
+noble.startScanning(scanForDevices, allowDoublicates); // any service UUID, no duplicates
+
+// var timerInst = setTimeout(function(){
+//   noble.stopScanning();
+//   console.log(count);
+//   process.exit(0);
+// }, 1000 * 20);
+
